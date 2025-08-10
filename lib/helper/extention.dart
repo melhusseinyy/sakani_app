6b7a5962c3e1e0f74d085f15d5b1887446a8cd4c
MohameeedWafik <mohamedwafik5445@gmail.com>
2025-08-10 10:56:57 +0300

    Revert "first commit"
    This reverts commit eb65897c88a2616474568ea0c438461975d2fd0a.

--- lib/helper/extention.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-extension SizedBoxExtension on num {
-  SizedBox get bh => SizedBox(height: toDouble().h);
-  SizedBox get bw => SizedBox(width: toDouble().w);
-  SizedBox get squareBox => SizedBox(width: toDouble().w, height: toDouble().h);
-}
--- lib/models/home_model/taskken_periods_model.dart
+++
@@ -1,9 +0,0 @@
-class TaskeenPeriodsModel {
-  final int? id;
-  final String? title;
-
-  TaskeenPeriodsModel(this.id, this.title);
-  factory TaskeenPeriodsModel.fromJson(Map<String, dynamic> json) {
-    return TaskeenPeriodsModel(json['id'], json['title']);
-  }
-}
